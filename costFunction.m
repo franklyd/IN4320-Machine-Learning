2300d429cea38880a6d1c3f6fede3d5f9246988f
franklyd <franklyd@163.com>
2017-02-21 21:54:47 +0100

    feat: add a new test case

--- costFunction.m
+++ costFunction.m
@@ -6,7 +6,8 @@ N0 = size(X0,1);
 N1= size(X1,1);
 M0 = theta(1:dim);
 M1 = theta(dim+1:end);
-jVal = sum(sum((X0 - repmat(M0,N0,1)).^2))+ sum(sum((X1 - repmat(M1,N1,1)).^2)) + lambda*sum(abs(M0-M1));
+%jVal = sum(sum((X0 - repmat(M0,N0,1)).^2))+ sum(sum((X1 - repmat(M1,N1,1)).^2)) + lambda*sum(abs(M0-M1));
+jVal = sum(sum((X0 - repmat(M0,N0,1)).^2))+ sum(sum((X1 - repmat(M1,N1,1)).^2)) + lambda*norm((M0-M1),1);
 %jVal = sum(sum((X0 - repmat(M0,N0,1)).^2))+ sum(sum((X1 - repmat(M0,N1,1)).^2));
 end
 
