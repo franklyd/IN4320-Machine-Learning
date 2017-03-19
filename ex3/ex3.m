3cb5091bc4e46745a6ab2f28c749853714e721d5
franklyd <franklyd@163.com>
2017-03-19 21:50:37 +0100

    debug: fixed some bugs

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -7,12 +7,28 @@ label = Data(:,58);
 X_dataset = prdataset(X_norm,label);
 
 %% basic LDA testing
-%[X_labeled,X_test] = gendat(X_dataset,[75,75]);
-X_unlabeled = gendat(X_test,[1000,1000]);
+[X_labeled,X_test] = gendat(X_dataset,[75,75]);
+X_unlabeled = gendat(X_test,[64,64]);
 X = [X_labeled; X_unlabeled];
-[mean0, mean1, sigma] = lda(X, 150, 0);
-[e, lab] = lda_predict(X_labeled,mean0,mean1,sigma);
-e
+[mean0, mean1, sigma] = lda(X, 150, 25);
+[e, lab] = lda_predict(X_test,mean0,mean1,sigma);
 %% Visualize Covariance Matrix
 imagesc(cov(X0));
 colorbar;
+
+%%
+e_history = zeros(8,10);
+unlabeled = [0, 10, 20, 40, 80, 160, 320, 640, 1280]; 
+[X_labeled,X_test] = gendat(X_dataset,[75,75]);
+for i=1:9
+    for j = 1:10
+        X_unlabeled = gendat(X_test,[unlabeled(i),unlabeled(i)]);
+        X = [X_labeled; X_unlabeled];
+        [mean0, mean1, sigma] = lda(X, 150, 200);
+        e = lda_predict(X_test,mean0,mean1,sigma);
+        e_history(i,j) = e; 
+    end
+end
+plot(unlabeled,e_history,'*')
+    
+    
--- ex3/lda.m
+++ ex3/lda.m
@@ -24,7 +24,7 @@ labeled_sigma0 = cov(X0);
 labeled_sigma1 = cov(X1);
 sigma0 = labeled_sigma0;
 sigma1 = labeled_sigma1;
-sigma = prior0 * sigma0 + prior1 * sigma1 + 1e-5*eye(D);
+sigma = prior0 * sigma0 + prior1 * sigma1 + 1e-10*eye(D);
 p0 = zeros(size(X_u,1),1);
 
 for i = 1: iterations
@@ -32,14 +32,12 @@ for i = 1: iterations
     % calculate post_Pr for unlabeled data
     p0_prev = p0;
     p0 = mvnpdf(X_u, mean0, sigma);
-    p0(p0==0) = eps(1);
     p1 = mvnpdf(X_u, mean1, sigma);
-    p1(p1==0) = eps(1);
-    if p1(p0==0) == 0
-        p0_temp = p0;
-        p0(p1==0)= 0.1;
-        p1(p0_temp==0) = 0.1;
-    end 
+    
+    p0_temp = p0;
+    p0(p1==0)= 0.1;
+    p1(p0_temp==0) = 0.1;
+    
     % normalize them
     p0 = (prior0 .* p0) ./(prior0 .* p0 + prior1 .* p1);
     p1 = 1 - p0;
@@ -64,7 +62,7 @@ for i = 1: iterations
     end
     sigma0 = sigma0 / (N0 + sum(p0) - 1);
     sigma1 = sigma1 / (N1 + sum(p1) -1 );
-    sigma = prior0 * sigma0 + prior1 * sigma1 + 1.0e-5*eye(D); 
+    sigma = prior0 * sigma0 + prior1 * sigma1 + 1.0e-10*eye(D); 
 end  
 end
 
